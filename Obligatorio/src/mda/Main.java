077dc472d577b0babc5db886fc116456ced712ea
manuel.dearmas <manuel.dearmas@globant.com>
2020-10-27 14:19:34 -0300

    initial commit

---
+++ Obligatorio/src/mda/Main.java
@@ -0,0 +1,10 @@
+package mda;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Everything in working order");
+	}
+
+}
